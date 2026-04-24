efbb1761c6aedec4ee716ee78792d817e72705a9
EllinaRoman <evelinka.late@yandex.ru>
2026-04-24 17:37:35 +0300

    Реализована навигация

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import './modules/form.js';
 import './modules/filtres.js';
 import './modules/modals.js';
 import './modules/status-glider.js';
+import './modules/navigation.js';
 import './modules/tags-trops.js';
 import './modules/theme.js';
 import './modules/ui-helpers.js';
@@ -9,6 +10,7 @@ import './modules/randomBook.js';
 import './modules/editBook.js';
 import './modules/storage.js';
 import './modules/library.js';
+
 
 
 import { renderBooks } from './modules/library.js';
---
+++ js/modules/navigation.js
@@ -0,0 +1,23 @@
+const navigation = document.querySelector('.navigation');
+const allTitles = navigation.querySelectorAll('.navigation_title');
+ const allContainers = document.querySelectorAll('.tab-content');
+
+navigation.addEventListener('click', (e) => {
+
+    const tab = e.target.closest('.navigation_title');
+        if (!tab) return;
+    if (tab.classList.contains('active_title')) return;
+
+
+    allTitles.forEach(el => el.classList.remove('active_title'));
+    tab.classList.add('active_title');
+
+    allContainers.forEach(container => container.classList.add('hidden'));
+    const targetId = tab.dataset.target;
+    const targetContainer = document.querySelector(`#${targetId}-content`);
+
+    if (targetContainer) {
+        targetContainer.classList.remove('hidden');
+    }
+
+})
